79e75cebb2c14eed05f0dd6a308820b712270266
Deepika-gowda246 <deepikaim246@gmail.com>
2022-12-09 13:28:17 +0530

    commit

---
+++ StudentDatabaseCRUD/Controllers/StudentController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentDatabaseCRUD.Models;
+
+namespace StudentDatabaseCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentsDatabaseContext _dbContext;
+
+        public StudentController(StudentsDatabaseContext context)
+        {
+            _dbContext = context;
+        }
+
+        //GET: api/Students
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudent()
+        {
+            return await _dbContext.Students.ToListAsync();
+        }
+
+        //GET: api/Students/1 (StudentId)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentByID(int id)
+        {
+
+            var student = await _dbContext.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        //POST: api/Students
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+            _dbContext.Students.Add(student);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction("GetStudent", new { id = student.StudentId }, student);
+        }
+
+
+        
+
+        //PUT: api/Students/2
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateStudent(int id, Student student)
+        {
+            if (id != student.StudentId)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+                return CreatedAtAction("GetStudent", new { id = student.StudentId }, student);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (IsStudentExist(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //DELETE: api/Students/2
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(int id)
+        {
+            var student = await _dbContext.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Students.Remove(student);
+            await _dbContext.SaveChangesAsync();
+
+            return student;
+        }
+
+        private bool IsStudentExist(int id)
+        {
+            return _dbContext.Students.Any(e => e.StudentId == id);
+        }
+
+    }
+}
+
---
+++ StudentDatabaseCRUD/Models/StudentsDatabaseContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentDatabaseCRUD.Models;
+
+public partial class StudentsDatabaseContext : DbContext
+{
+    
+
+    public StudentsDatabaseContext(DbContextOptions<StudentsDatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+   
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK__Student__32C52A79F95BF1CA");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.StudentId).HasColumnName("StudentID");
+            entity.Property(e => e.Address)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Class)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
